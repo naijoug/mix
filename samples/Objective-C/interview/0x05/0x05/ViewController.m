7b4a7cd062860e4dbbc9fd5c33ddcbabd4b7b43d
naijoug <naijoug@outlook.com>
2024-03-20 15:31:29 +0800

    update interview code

--- samples/Objective-C/interview/0x05/0x05/ViewController.m
+++ samples/Objective-C/interview/0x05/0x05/ViewController.m
@@ -8,7 +8,7 @@
 #import "ViewController.h"
 
 @interface Person : NSObject
-@property(nonatomic,copy) NSString *name;
+@property(nonatomic, copy) NSString *name;
 @end
 @implementation Person
 - (void)speak {
@@ -20,18 +20,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    
+//    NSString *str = @"test";
+    
     id cls = [Person class];
     void *obj = &cls;
     [(__bridge id)obj speak];
+    
+    NSLog(@"%@ - %@", cls, obj);
+    //NSLog(@"cls address:%p str address:%p", &cls, &str);
 }
-
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
 }
-
 
 @end
--- samples/Objective-C/interview/0xt06/0xt06/ViewController.m
+++ samples/Objective-C/interview/0xt06/0xt06/ViewController.m
@@ -11,6 +11,8 @@
 
 @property (nonatomic, strong) NSString *target;
 
+@property (atomic, strong) NSString *string;
+
 @end
 
 @implementation ViewController
@@ -18,11 +20,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [self question]; // 崩溃
+    
+    [self solutionInTaggedPointer];
+    [self solutionInAtomic];
+}
+
+- (void)question {
     dispatch_queue_t queue = dispatch_queue_create("parallel", DISPATCH_QUEUE_CONCURRENT);
     for (int i = 0; i < 1000000 ; i++) {
         dispatch_async(queue, ^{
-//             self.target = [NSString stringWithFormat:@"ksddkjalkjd%d",i];
+            self.target = [NSString stringWithFormat:@"ksddkjalkjd%d",i];
+        });
+    }
+}
+
+- (void)solutionInTaggedPointer {
+    dispatch_queue_t queue = dispatch_queue_create("parallel", DISPATCH_QUEUE_CONCURRENT);
+    for (int i = 0; i < 1000000 ; i++) {
+        dispatch_async(queue, ^{
             self.target = [NSString stringWithFormat:@"kd%d",i];
+        });
+    }
+}
+
+- (void)solutionInAtomic {
+    dispatch_queue_t queue = dispatch_queue_create("parallel", DISPATCH_QUEUE_CONCURRENT);
+    for (int i = 0; i < 1000000 ; i++) {
+        dispatch_async(queue, ^{
+            self.string = [NSString stringWithFormat:@"ksddkjalkjd%d",i];
         });
     }
 }
